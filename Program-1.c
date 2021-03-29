65eca0a37f436fa378c05bc360759d21e8af146f
KAMAL <20me0044@vvce.ac.in>
2021-03-29 11:11:31 +0000

    done

--- Program-1.c
+++ Program-1.c
@@ -10,12 +10,12 @@ struct Distance
 
 int main()
 {
-    printf("Enter feet  value for 1st and 2nd distance: ");
+   
     scanf("%d%d", &dist1.feet, &dist2.feet);
     // adding feet
     sum.feet = dist1.feet + dist2.feet;
    
 
-    printf("Sum of feet = ",sum.feet);//Enter the missing code
+    printf("Sum of feet = %d",sum.feet);//Enter the missing code
     return 0;
 }
